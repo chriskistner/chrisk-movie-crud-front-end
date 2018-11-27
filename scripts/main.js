14a0f150b84d638788d8b2b63b57533b71c99f1d
Christopher Kistner <chris.kistner0@gmail.com>
2018-11-27 15:30:24 -0800

    Completed basic setup for movies

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,38 @@ const apiURLActors = "http://localhost:3000/actors";
 const axios = require('axios');
 const create = require('./templates');
 
+function getMovies() { axios.get(apiURLMovies) 
+    .then(function (result) {
+        let movies = result.data;
+        populatePage(movies);
+    })
+};
+getMovies();
+
+function populatePage(arr){
+    const appliedTemplates = arr.map(film => create.movieRow(film.id, film.title, film.released, film.director, film.rating, film.poster)).join('\n');
+    document.querySelector(".main-body").innerHTML = appliedTemplates;
+
+    for (let film of arr){
+        let deleteBlogButton = document.querySelector(`#delete-film[data-id="${film.id}"]`);
+        let updateBlogButton = document.querySelector(`#edit-film[data-id="${film.id}"]`);
+        let closeUpdateButton = document.querySelector(`#stop-post[data-id="${film.id}"]`);
+        let updateField = document.querySelector(`.update-field[data-id="${film.id}"]`);
+        deleteBlogButton.addEventListener('click', function(){
+            axios.delete(apiURLMovies+`/${film.id}`)
+            .then(function(){
+                console.log("Film Deleted")
+                getMovies();
+            })
+        });
+
+        updateBlogButton.addEventListener('click', function() {
+            updateField.classList.remove('hide-menu')
+        });
+
+        closeUpdateButton.addEventListener('click', function(){
+            updateField.classList.add('hide-menu')
+        });
+
+    }  
+};
--- scripts/templates.js
+++ scripts/templates.js
@@ -1,25 +1,82 @@
-const movieRow =(title, released, director, rating, poster,) => {
+const movieRow =(id, title, released, director, rating, poster,) => {
     return `
-    <div class="row blog-row">
-        <div class="col-4 border">
-            <img src="${poster}" height="42">
+    <div class="row border">
+        <div class="col-2 border">
+            <img src="${poster}" height="200px">
         </div>
         <div class="col-2">
-        <p style="margin-bottom: 0px;">Title: ${title}</p>
+            <p style="margin-bottom: 0px;">Title: ${title}</p>
         </div>
         <div class="col-2">
-        <p style="margin-bottom: 0px;">Released: ${released}</p>
+            <p style="margin-bottom: 0px;">Released: ${released}</p>
         </div>
         <div class="col-2">
-        <p style="margin-bottom: 0px;">Director: ${director}</p>
+            <p style="margin-bottom: 0px;">Director: ${director}</p>
         </div>
         <div class="col-2">
-        <p style="margin-bottom: 0px;">Rating: ${rating}</p>
+            <p style="margin-bottom: 0px;">Rating: ${rating}</p>
+        </div>
+        <div class="col-2">
+        <div class="blog-box">
+        <button id="edit-film" data-id="${id}" type="button" class="btn btn-outline-danger">Update</button>
+        <button id="delete-film" data-id="${id}" type="button" class="btn btn-outline-warning">Delete</button>
+    </div>
+        </div>
+    </div>
+    <div class="row update-field hide-menu border" data-id="${id}">
+        <div class="col-12">
+            <form id="submitUpdatedPost">
+                <button id = "stop-post" data-id="${id}" type="button" class="btn btn-outline-dark">X</button>
+                <input class="input-fieldB" type="text" id="updateTitle" value="${poster}">
+                <input class="input-fieldA" type="text" id="updateTitle" value="${title}">
+                <input class="input-fieldB" type="text" id="updateTitle" value="${released}">
+                <input class="input-fieldC" type="text" id="updateTitle" value="${director}">
+                <input class="input-fieldD" type="text" id="updateTitle" value="${rating}">
+                <input type="submit" id="submitUpdate" value="Update">
+            </form>
         </div>
     </div>
     `
 };
 
+const newMovie = () => {
+    return `
+        <div class="form-group">
+            <form id="submitPost">
+              <div class="row align-items-center justify-content-between">
+                  <div class="col-12">
+                      <h2>Movie Information</h2>
+                  </div>
+                </div>
+                <div class="menuBar">
+                <input class="input-fieldE" type="image" id="filmPoster" src="https://via.placeholder.com/275" alt="photo">
+
+                    <label class="menuBar" for="filmTitle">Movie Title</label>
+                </div>
+                    <input class="input-fieldA" type="text" id="filmTitle">
+
+                <div class="menuBar">
+                    <label class="menuBar" for="filmRelease">Release Year</label>
+                </div>
+                    <input class="input-fieldB" type="text" id="filmRelease">
+
+                <div class="menuBar">
+                    <label class="menuBar" for="filmDirector">Director</label>
+                </div>
+                <input class="input-fieldC" type="text" id="filmDirector">
+
+                <div class="menuBar">
+                    <label class="menuBar" for="filmRating">Star Rating</label>
+                </div>
+                <input class="input-fieldD" type="text" id="filmRating">
+
+                <input type="submit" id="submission" value="SUBMIT">
+            </form>
+        </div>
+    `
+}
+
 module.exports = {
     movieRow,
+    newMovie
 }
