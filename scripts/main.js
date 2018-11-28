28869c8674bd128872f83888b891cf3f1984085d
Christopher Kistner <chris.kistner0@gmail.com>
2018-11-28 15:06:45 -0800

    Completed all basic functionality for the Movie CRUD features

--- scripts/main.js
+++ scripts/main.js
@@ -3,26 +3,40 @@ const apiURLActors = "http://localhost:3000/actors";
 const axios = require('axios');
 const create = require('./templates');
 
+//ALL MOVIES PAGE
 function getMovies() { axios.get(apiURLMovies) 
     .then(function (result) {
         let movies = result.data;
-        populatePage(movies);
+        populateMovies(movies);
     })
 };
-getMovies();
 
-function populatePage(arr){
+function populateMovies(arr){
     const appliedTemplates = arr.map(film => create.movieRow(film.id, film.title, film.released, film.director, film.rating, film.poster, film.actors)).join('\n');
     document.querySelector(".main-body").innerHTML = appliedTemplates;
-
+    
     for (let film of arr){
         const appliedLists = film.actors.map(actor => create.createActorList(actor.first_name, actor.last_name)).join('\n');
-        document.querySelector(`#actor-list[data-id="${film.id}"]`).innerHTML = appliedLists;
-        console.log(appliedLists);
+        
+        if (appliedLists.length === 0) {
+            document.querySelector(`#actor-list[data-id="${film.id}"]`).innerHTML = "<li><i>NO ACTORS LISTED</i></li>";
+        } else {document.querySelector(`#actor-list[data-id="${film.id}"]`).innerHTML = appliedLists}
+        
+
+        // MOVIE ROW MENU BUTTONS
         let deleteBlogButton = document.querySelector(`#delete-film[data-id="${film.id}"]`);
         let updateBlogButton = document.querySelector(`#edit-film[data-id="${film.id}"]`);
+
+        // UPDATE FIELD AREA
         let closeUpdateButton = document.querySelector(`#stop-post[data-id="${film.id}"]`);
         let updateField = document.querySelector(`.update-field[data-id="${film.id}"]`);
+        let updateFieldPoster = document.querySelector(`#update-poster[data-id="${film.id}"]`);
+        let updateFieldTitle = document.querySelector(`#update-title[data-id="${film.id}"]`);
+        let updateFieldReleased = document.querySelector(`#update-released[data-id="${film.id}"]`);
+        let updateFieldDirector = document.querySelector(`#update-director[data-id="${film.id}"]`);
+        let updateFieldRating = document.querySelector(`#update-rating[data-id="${film.id}"]`);
+        let submitUpdate = document.querySelector(`#submit-updated-post[data-id="${film.id}"]`);
+
         deleteBlogButton.addEventListener('click', function(){
             axios.delete(apiURLMovies+`/${film.id}`)
             .then(function(){
@@ -32,12 +46,82 @@ function populatePage(arr){
         });
 
         updateBlogButton.addEventListener('click', function() {
-            updateField.classList.remove('hide-menu')
+            if(updateField.classList.contains('hide-menu')) {
+                updateField.classList.remove('hide-menu')
+            } else {updateField.classList.add('hide-menu')}
         });
 
         closeUpdateButton.addEventListener('click', function(){
             updateField.classList.add('hide-menu')
         });
 
+        submitUpdate.addEventListener('submit', function(){
+            event.preventDefault();
+            const updateMovie = axios.put(apiURLMovies+`/${film.id}`, {
+                poster: updateFieldPoster.value,
+                title: updateFieldTitle.value,
+                released: updateFieldReleased.value,
+                director: updateFieldDirector.value,
+                rating: updateFieldRating.value,
+            })
+            .then(function(){
+                console.log('Updated');
+                getMovies();
+                updateField.classList.add('hide-menu');
+            })
+        })
+
     }  
 };
+
+//ADD MOVIE PAGE
+function populateAddMovie () {
+    document.querySelector(".main-body").innerHTML = create.newMovie();
+
+    let addNewMovie= document.querySelector(`#submit-movie`);
+    let newMovieLink= document.querySelector(`#filmPoster`);
+    let newMovieTitle= document.querySelector(`#filmTitle`);
+    let newMovieRelease= document.querySelector(`#filmRelease`);
+    let newMovieDirector= document.querySelector(`#filmDirector`);
+    let newMovieRating= document.querySelector(`#filmRating`);
+
+    addNewMovie.addEventListener('submit', function() {
+        event.preventDefault();
+        const addMovie = axios.post(apiURLMovies, {
+            poster : newMovieLink.value,
+            title: newMovieTitle.value,
+            released: newMovieRelease.value,
+            director: newMovieDirector.value,
+            rating: newMovieRating.value
+        })
+        .then(function(){
+            console.log("Success")
+            console.log(newMovieLink.value);
+            document.querySelector(".main-body").innerHTML = create.displayNewMovie(newMovieTitle.value, newMovieLink.value, newMovieRelease.value, newMovieDirector.value, newMovieRating.value) 
+        })
+    })
+
+}
+
+let searchForFilm = document.querySelector('#movie-search');
+
+function getMovie(film) {
+    axios.get(apiURLMovies + `/${film}`)
+        .then(function (result) {
+            let film = result.data[0];
+            populateMovies([film]);
+        })
+}
+
+searchForFilm.addEventListener('submit', function(event){
+    event.preventDefault();
+    let targetMovie = event.target.searchField.value;
+    getMovie(targetMovie);
+    event.target.searchField.value = '';
+})
+
+if (window.location.href.endsWith('/index.html')){
+    getMovies();
+} else if (window.location.href.endsWith('/add-movie.html')) {
+    populateAddMovie();
+}
--- scripts/templates.js
+++ scripts/templates.js
@@ -1,38 +1,37 @@
-const movieRow =(id, title, released, director, rating, poster, actors) => {
+const movieRow =(id, title, released, director, rating, poster) => {
     return `
-    <div class="row border">
-        <div class="col-2 border">
+    <div class="row justify-content-center item-rows">
+        <div class="col-2 border-top border-left border-bottom">
             <img src="${poster}" height="200px">
         </div>
-        <div class="col-2">
+        <div class="col-2 border-top border-bottom">
+            <p style="margin-bottom: 0px;">Site ID: ${id}</p>
             <p style="margin-bottom: 0px;">Title: ${title}</p>
             <p style="margin-bottom: 0px;">Released: ${released}</p>
+            <p style="margin-bottom: 0px;">Rating: ${rating}</p>
             
         </div>
 
-        <div class="col-2">
+        <div class="col-3 border-top border-bottom">
             <p style="margin-bottom: 0px;">Actors:<ul id="actor-list" data-id=${id}> </ul></p>
         </div>
-        <div class="col-2">
-            <p style="margin-bottom: 0px;">Rating: ${rating}</p>
-        </div>
-        <div class="col-2">
+        <div class="col-2 border-top border-right border-bottom">
         <div class="blog-box">
         <button id="edit-film" data-id="${id}" type="button" class="btn btn-outline-danger">Update</button>
         <button id="delete-film" data-id="${id}" type="button" class="btn btn-outline-warning">Delete</button>
     </div>
         </div>
     </div>
-    <div class="row update-field hide-menu border" data-id="${id}">
-        <div class="col-12">
-            <form id="submitUpdatedPost">
+    <div class="row update-field hide-menu border justify-content-center" data-id="${id}">
+        <div class="col-8">
+            <form id="submit-updated-post" data-id="${id}">
                 <button id = "stop-post" data-id="${id}" type="button" class="btn btn-outline-dark">X</button>
-                <input class="input-fieldB" type="text" id="updateTitle" value="${poster}">
-                <input class="input-fieldA" type="text" id="updateTitle" value="${title}">
-                <input class="input-fieldB" type="text" id="updateTitle" value="${released}">
-                <input class="input-fieldC" type="text" id="updateTitle" value="${director}">
-                <input class="input-fieldD" type="text" id="updateTitle" value="${rating}">
-                <input type="submit" id="submitUpdate" value="Update">
+                <input class="input-fieldE" type="text" data-id="${id}"  id="update-poster" value="${poster}">
+                <input class="input-fieldA" type="text" data-id="${id}"  id="update-title" value="${title}">
+                <input class="input-fieldB" type="text" data-id="${id}"  id="update-released" value="${released}">
+                <input class="input-fieldC" type="text" data-id="${id}"  id="update-director" value="${director}">
+                <input class="input-fieldD" type="text" data-id="${id}"  id="update-rating" value="${rating}">
+                <input type="submit" id="submit-update" data-id="${id}" value="Update Movie">
             </form>
         </div>
     </div>
@@ -43,26 +42,36 @@ const createActorList = (fName, lName) => {
     return `
     <li>${fName} ${lName}</li>
     `
-}
+};
 
 const newMovie = () => {
     return `
-        <div class="form-group">
-            <form id="submitPost">
-              <div class="row align-items-center justify-content-between">
-                  <div class="col-12">
-                      <h2>Movie Information</h2>
-                  </div>
+        <div class="row justify-content-center item-rows">
+            <div class="col-4 border">
+                <div class="form-group">
+                <form id="submit-movie">
+                    <div class="row align-items-center justify-content-between">
+                        <div class="col">
+                            <h2>Movie Information</h2>
+                        </div>
+                    </div>
+                <div class="row justify-content-center">
+                    <div class="col-sm">
+                        <img class="input-fieldE" src="https://via.placeholder.com/150" alt="poster">
+                    </div>
                 </div>
-                <div class="menuBar">
-                <input class="input-fieldE" type="image" id="filmPoster" src="https://via.placeholder.com/275" alt="photo">
 
+                <div class="menuBar">
+                    <label class="menuBar" for="filmPoster">Movie Poster <i>link Only</i></label>
+                </div>
+                    <input class="input-fieldE" type="text" id="filmPoster">
+                <div class="menuBar">
                     <label class="menuBar" for="filmTitle">Movie Title</label>
                 </div>
                     <input class="input-fieldA" type="text" id="filmTitle">
 
                 <div class="menuBar">
-                    <label class="menuBar" for="filmRelease">Release Year</label>
+                    <label class="menuBar" for="filmRelease">Year Released</label>
                 </div>
                     <input class="input-fieldB" type="text" id="filmRelease">
 
@@ -74,16 +83,61 @@ const newMovie = () => {
                 <div class="menuBar">
                     <label class="menuBar" for="filmRating">Star Rating</label>
                 </div>
-                <input class="input-fieldD" type="text" id="filmRating">
+                    <input class="input-fieldD" type="text" id="filmRating">
+                    <input type="submit" id="submission" value="SUBMIT">
+                </form>
+            </div>
+       </div> 
+       </div>
+    `
+};
 
-                <input type="submit" id="submission" value="SUBMIT">
-            </form>
-        </div>
+const displayNewMovie = (title, poster,released, director, rating) => {
+    return `
+    <div class="row justify-content-center item-rows">
+        <div class="col-4 border">
+            <div class="row align-items-center">
+                <div class="col-12">
+                    <h4>New Movie Information</h4>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-12 border">
+                    <img class="sample-poster border" src="${poster}" alt="poster">
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <h3>
+                    Title: 
+                    <small class="text-muted"> ${title}</small>
+                </h3>
+            </div>
+            <div class="row justify-content-center">
+                <h3>
+                    Release Year:  
+                    <small class="text-muted"> ${released}</small>
+                </h3>
+            </div>
+            <div class="row justify-content-center">
+                <h3>
+                    Directed By:  
+                    <small class="text-muted"> ${director}</small>
+                </h3>
+            </div>
+            <div class="row justify-content-center">
+                <h3>
+                    Your Rating:  
+                    <small class="text-muted"> ${rating} Stars</small>
+                </h3>
+            <div>
+        </div
+    </div>
     `
 }
 
 module.exports = {
     movieRow,
     newMovie,
-    createActorList
-}
+    createActorList,
+    displayNewMovie
+};
