d5ce6c50b6d2d74eac0d00821412d73245442c54
Christopher Kistner <chris.kistner0@gmail.com>
2018-12-10 08:34:38 -0800

    Attempting to fix homepage load after Surge

--- scripts/main.js
+++ scripts/main.js
@@ -270,7 +270,7 @@ if (window.location.href.endsWith('/movies.html')){
 } else if (window.location.href.endsWith('/add-actor.html')) {
     populateAddActorPage();
 
-} else if (window.location.href.endsWith('/index.html')) {
+} else if (window.location.href.endsWith('.sh')) {
     const homePageTemplate = create.displayHomePageMenu();
     document.querySelector(".main-body").innerHTML = homePageTemplate;
 }
